1f8eae8479eca48edf0ac2b1afe9c1b063155c45
Max Goodman <max.goodman@flatironschool.com>
2019-02-06 17:28:50 -0500

    adding comment on second test case

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,7 +2,9 @@ const assert = require('assert'); // standard library, for making assertions abo
 const ganache = require('ganache-cli'); //requiring ganache
 const Web3 = require('web3'); //uppercase Web3 notice this, is a constructor used to create instances of the web3 library , think of it as the class of Web3
 
-const web3 = new Web3(ganache.provider()); //instance of Web3 and connect to local test network (ganache)
+//updating lines for web3 breaking changes
+const provider = ganache.provider();
+const web3 = new Web3(provider); //instance of Web3 and connect to local test network (ganache)
 
 const { interface, bytecode } = require('../compile'); //import the interface and bytecode of the contract
 
@@ -47,10 +49,17 @@ beforeEach(async()=>{ //add async
     inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })//initial argument is the bytecode of the contract, second argument
     .send({ from: accounts[0], gas: '1000000' }) //specifies the account from which to deploy and gas to use for the deployment of the contract
+
+    //ADD THIS ONE LINE RIGHT HERE!!!
+    inbox.setProvider(provider)
 })
 
 describe('Inbox', () =>{
     it('deploys a contract', () =>{
         assert.ok(inbox.options.address); // ok is a part of the node standard library, makes assertion that it is a value that it exists.  looking up if it exists then the contract has been successfully deployed. if it is a truthy value, then the test will pass because a string is a truthy value. 
     });
-})
+    it('has a default message', async () =>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Hi there!");
+    });
+});
