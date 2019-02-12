282e00b94c0b1c4c4e002877d5a68ba5d48091c8
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 12:43:32 -0500

    adding beforeEach

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -6,3 +6,13 @@ const web3 = new Web3(ganache.provider());//provider allows us to connect to any
 
 const { interface, bytecode } = require('../compile');
 
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+})
